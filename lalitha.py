fbf13c3969b53ab15a79792437c956914b7ee8f0
lalithagonnuri <140973108+lalithagonnuri@users.noreply.github.com>
2024-01-12 10:36:46 +0530

    Update lalitha.py

--- lalitha.py
+++ lalitha.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.header('puppy')
 st.write('puppy')
-st.image('https://e0.pxfuel.com/wallpapers/944/816/desktop-wallpaper-baby-puppies-cute-baby-puppy.jpg'),caption='puppy')
+st.image('https://e0.pxfuel.com/wallpapers/944/816/desktop-wallpaper-baby-puppies-cute-baby-puppy.jpg,caption='puppy')
